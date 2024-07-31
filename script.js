33f7cd071d070101ff5610d9a538c38acde727a1
edzhang10 <edzhang10@gmail.com>
2024-07-31 10:59:53 -0400

    figma and cursor/bg

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('hi')
+
+addEventListener('mousemove', (event) => {
+    let x = event.clientX;
+    let y = event.clientY;
+  
+    const cursorInfo = document.getElementById('cursor');
+    cursorInfo.style.top = y-6 + 'px';
+    cursorInfo.style.left = x-4 + 'px';
+  
+    const cursorOutlineInfo = document.getElementById('cursor-outline');
+    cursorOutlineInfo.style.top = y-15 + 'px';
+    cursorOutlineInfo.style.left = x-11.5 + 'px'
+    cursorOutlineInfo.style.transform = `translate(${clientX}px, ${clientY}px)`;
+}) 
