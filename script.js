b591564e9b42f4b30234f92e86f2c668c0c74fac
edzhang10 <edzhang10@gmail.com>
2024-08-04 12:21:14 -0400

    blog search bar and better handling

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('hi')
-
 addEventListener('mousemove', (event) => {
     let x = event.pageX;
     let y = event.pageY;
@@ -21,12 +19,12 @@ addEventListener('click', async (event) => {
 })
 
 addEventListener('DOMContentLoaded', (event) => {
+    if (window.location.pathname == "/projects.html") {
     const projectsSearch = document.getElementById('projects-search');
     const allProjects = Array.from(document.getElementsByClassName('project'));
     let removedNodes = [];
     projectsSearch.addEventListener('input', (event) => {
         const projectSearchValue = event.target.value;
-        const projects = document.getElementsByClassName('project');
         for (project of allProjects) {
             project_name = project.innerText;
             if (project_name.includes(projectSearchValue)) {
@@ -42,5 +40,45 @@ addEventListener('DOMContentLoaded', (event) => {
 
             }
         }
-})
+    })
+} else if (window.location.pathname == '/blog.html') {
+    const blogSearch = document.getElementById('blog-search');
+    const allBlogs = Array.from(document.getElementsByClassName('blog'));
+    const defaultBlogs = Array.from(document.getElementById('post-box').children);
+    console.log(defaultBlogs)
+    let removedBlogs = [];
+    inDefault = true;
+    blogSearch.addEventListener('input', (event) => {
+        const blogSearchValue = event.target.value;
+        if (blogSearchValue == '') {
+            for (b of defaultBlogs) {
+                topList = document.getElementById('post-box');
+                topList.appendChild(b);
+                inDefault = true;
+            }
+        } else {
+            if (inDefault == true) {
+                document.querySelectorAll('.post-date').forEach(e => e.remove());
+                document.getElementById('first-post-date').remove();
+            }
+            inDefault = false;
+            for (blog of allBlogs) {
+                blog_name = blog.innerText;
+                if (blog_name.includes(blogSearchValue)) {
+                    for (p of removedBlogs) {
+                        if (p == blog_name) {
+                            topList = document.getElementById("post-box");
+                            topList.appendChild(blog);
+                        }
+                }
+                } else {
+                    removedBlogs.push(blog_name);
+                    console.log(blog);
+                    blog.remove();
+    
+                }
+            }
+        }
+    })
+}
 })
